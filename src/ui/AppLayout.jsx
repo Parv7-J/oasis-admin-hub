3512d6882d80473512bded767afbcf0fd5b594a5
Parv7-J <parvjain19012006@gmail.com>
2025-01-11 22:34:31 +0530

    ko

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./Header";
-import Sidebar from "./SideBar";
+import Sidebar from "./SideBar.jsx";
 
 const StyledAppLayout = styled.div`
   display: grid;
